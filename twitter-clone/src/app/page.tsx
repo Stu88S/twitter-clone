a2694543cb50fae2689bcd0b61208834539eb15e
Stu88S <stu88s.tor@gmail.com>
2023-08-18 13:21:06 +0400

    Home bar finished

--- twitter-clone/src/app/page.tsx
+++ twitter-clone/src/app/page.tsx
@@ -1,5 +1,7 @@
 import LeftSidebar from "@/components/LeftSidebar";
-import { BsThreeDots } from "react-icons/bs";
+import { BsDot, BsThreeDots, BsChat } from "react-icons/bs";
+import { AiOutlineHeart, AiOutlineRetweet } from "react-icons/ai";
+import { IoStatsChart, IoShareOutline } from "react-icons/io5";
 
 const Home = () => {
 	return (
@@ -7,8 +9,8 @@ const Home = () => {
 			<div className="max-w-screen-xl w-full h-full flex relative">
 				{/* LEFT SIDEBAR FOR NAVIGATION */}
 				<LeftSidebar />
-				<main className="ml-[320px] mx-2 flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
-					<h1 className="text-xl font-bold p-6">Home</h1>
+				<main className="ml-[320px] mx-2 flex w-full max-w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
+					<h1 className="text-xl font-bold p-6 backdrop-blur bg-black/10 sticky top-0">Home</h1>
 					<div className="flex items-stretch px-4 py-4 space-x-2 border-t-[0.5px] border-b-[0.5px] border-gray-600 relative">
 						<div className="w-10 h-10 bg-slate-400 rounded-full flex-none"></div>
 						<div className="flex flex-col w-full h-full">
@@ -29,34 +31,49 @@ const Home = () => {
 					</div>
 					<div className="flex flex-col">
 						{Array.from({ length: 5 }).map((_, i) => (
-							<div key={i} className="p-4 border-t-[0.5px] border-b-[0.5px] flex space-x-4">
+							<div key={i} className="py-4 px-6 border-b-[0.5px] border-gray-600 flex space-x-4">
 								<div>
 									<div className="w-10 h-10 bg-slate-200 rounded-full"></div>
 								</div>
 								<div>
-									<div className="flex flex-col space-y-4">
-										<div className="flex items-center space-x-1">
-											<div className="font-bold">Club of Coders</div>
-											<div>@clubofcoders</div>
-											<div>1 hour ago</div>
-											<div>
+									<div className="flex flex-col space-y-2">
+										<div className="flex items-center justify-between w-full">
+											<div className="flex items-center space-x-1 w-full">
+												<div className="font-bold">Club of Codersd</div>
+												<div className="text-gray-500">@clubofcoders</div>
+												<div className="text-gray-500">
+													<BsDot />
+												</div>
+												<div className="text-gray-500">1 hour ago</div>
+											</div>
+											<div className="">
 												<BsThreeDots />
 											</div>
 										</div>
-										<div className="text-white text-sm">
+										<div className="text-white text-base">
 											Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eveniet, reiciendis, beatae sequi modi provident optio,
 											eaque pariatur omnis ea repellat ratione voluptate voluptates sapiente. In vel deserunt eligendi fugiat, consectetur
 											omnis expedita aspernatur animi voluptatum velit ratione a rerum labore nisi minima, laborum exercitationem aliquid,
 											harum ipsam nesciunt. Eius et similique sunt quam odio, earum minus?
 										</div>
 
-										<div className="bg-slate-400 aspect-square w-full h-96 rounded-xl"></div>
-										<div className="flex items-center space-x-2 w-full">
-											<div>C</div>
-											<div>R</div>
-											<div>L</div>
-											<div>S</div>
-											<div>SH</div>
+										<div className="bg-slate-400 aspect-square w-full h-80 rounded-xl "></div>
+										<div className="flex items-center justify-around w-full pt-2">
+											<div className="rounded-full hover:bg-white/10 transition duration-200 p-2 cursor-pointer">
+												<BsChat />
+											</div>
+											<div className="rounded-full hover:bg-white/10 transition duration-200 p-2 cursor-pointer">
+												<AiOutlineRetweet />
+											</div>
+											<div className="rounded-full hover:bg-white/10 transition duration-200 p-2 cursor-pointer">
+												<AiOutlineHeart />
+											</div>
+											<div className="rounded-full hover:bg-white/10 transition duration-200 p-2 cursor-pointer">
+												<IoStatsChart />
+											</div>
+											<div className="rounded-full hover:bg-white/10 transition duration-200 p-2 cursor-pointer">
+												<IoShareOutline />
+											</div>
 										</div>
 									</div>
 								</div>
